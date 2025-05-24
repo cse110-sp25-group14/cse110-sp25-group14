9e931ae3b7066098de7123c793c2b6724bad5ad8
j5995 <wangtianyi63@gmail.com>
2025-05-24 04:41:05 -0700

    Finished sequence basic game function

--- scripts/sequence.js
+++ scripts/sequence.js
@@ -7,7 +7,6 @@ function init() {
 		window.location.href = "homepage.html";
 	});
 	initializeCardList();
-	playCards();
 };
 
 let cardList = [];
@@ -21,9 +20,11 @@ function initializeCardList(){
 			cardList.push(cardElement);
 		}
 	}
+    playCards();
 }
 
 let cards = [];
+let currPointer = 0;
 
 function appendRandom(array){
 	array.push(Math.floor(Math.random() * 8));
@@ -31,18 +32,53 @@ function appendRandom(array){
 
 const timer = ms => new Promise(res => setTimeout(res, ms));
 
-
 async function playCards(){
 	appendRandom(cards);
 	for(let j = 0; j<cards.length; j+=1){
 		const cardElement = cardList[cards[j]];
+        cardElement.removeEventListener("click", checkClicked, false);
 		cardElement.classList.add("unflipped");
+        flipCard(cardElement);
 		setTimeout(()=>{
 			flipCard(cardElement);
 		}, 500);
-		flipCard(cardElement);
 		await timer(1000);
 	}
+    for(let i = 0; i<cardList.length; i+=1){
+       	const cardElement = cardList[i]; 
+        cardElement.addEventListener("click", checkClicked);
+    }
+}
+
+async function checkClicked(){
+    this.classList.add("unflipped");
+    if(this != cardList[cards[currPointer]]){
+        endGame();
+        setTimeout(()=>{
+		    flipCard(this);
+	    }, 500);
+	    flipCard(this);
+        return;
+    }
+	setTimeout(()=>{
+		flipCard(this);
+	}, 500);
+	flipCard(this);
+	await timer(500);
+    
+    currPointer += 1;
+    if(currPointer == cards.length){
+        currPointer = 0;
+        playCards();
+    }
+}
+
+function endGame(){
+    for(let i = 0; i<cardList.length; i+=1){
+       	const cardElement = cardList[i]; 
+        cardElement.removeEventListener("click", checkClicked, false);
+    }
+    console.log("game end");
 }
 
 function flipCard(card){
