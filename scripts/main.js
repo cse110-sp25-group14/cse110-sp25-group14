b276c5d75dcdc534cfeaf57afabc607077bcc6a7
j5995 <wangtianyi63@gmail.com>
2025-05-23 17:45:13 -0700

    Initialized scripts folder and leaderboard.html

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+// run everything after page loads
+window.addEventListener('DOMContentLoaded', init);
+
+// Starts program, all functions calls originate here
+async function init() {
+	//insert functions/eventListeners to functions
+}
