5974e633a99ac8bdeb7dc5c12165b332daf45f4d
Andy <kgh3122@gmail.com>
2025-05-24 22:30:52 -0700

    add_yml_for_jsdocs

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -49,7 +49,12 @@ function init() {
 	letsFindOutBtn.addEventListener("click", random_game_page);
 }
 
-
+/**
+ * @function randomgame_page
+ * @description
+ * This function redirects the user to the leaderboard page.
+ * This function is called when the user clicks on the leaderboard button
+ */
 function random_game_page() {
 	const games = ["matching.html", "sequence.html"];
 	const randomIndex = Math.floor(Math.random() * games.length);
