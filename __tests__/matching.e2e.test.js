6f531a80e92a4b59fc796ee69b1f9d9ca428f1a4
yscmark <124672723+yscmark@users.noreply.github.com>
2025-06-01 00:44:18 -0700

    Created partial E2E test

--- __tests__/matching.e2e.test.js
+++ __tests__/matching.e2e.test.js
@@ -1,8 +1,40 @@
-beforeAll(async () => {
-	await page.goto("http://localhost:3000/matching.html");
+describe("basic user flow", () => {	
+	test("homepage should have navigation to both games", async () => {
+		await page.goto("http://localhost:3000/homepage.html");
+
+		const matchingButton = await page.$("#matching-button");
+		expect(matchingButton).not.toBeNull();
+
+		await Promise.all([
+			page.waitForNavigation(),
+			page.click("#matching-button"),
+		]);
+		expect(page.url()).toMatch("http://localhost:3000/matching.html");
+
+
+		await page.goto("http://localhost:3000/homepage.html");
+
+		const sequenceButton = await page.$("#sequence-button");
+		expect(sequenceButton).not.toBeNull();
+
+		await Promise.all([
+			page.waitForNavigation(),
+			page.click("#sequence-button"),
+		]);
+		expect(page.url()).toBe("http://localhost:3000/sequence.html");
+	});
+
+	test("matching game should allow interaction", async () => {
+		await page.goto("http://localhost:3000/matching.html");
+
+		const cards = await page.$$(".card");
+		expect(cards.length).toBe(16);
+
+		// await page.click("#start-btn");
+
+		// await cards[0].click();
+		// const flippedCard = await page.$(".flipped");
+		// expect(flippedCard).toBe(cards[0]);
+	});
 });
 
-test("placeholder", async () => {
-	const startButton = await page.$("#start-btn");
-	expect(startButton).toBeDefined();
-});
