1bbf2d13b5037c8f128d48da649ef4afb4b788eb
yscmark <124672723+yscmark@users.noreply.github.com>
2025-06-03 18:15:08 -0700

    E2E test now expects that, in both games, clicking a card changes the state of the card

--- __tests__/homepage.e2e.test.js
+++ __tests__/homepage.e2e.test.js
@@ -1,5 +1,5 @@
 describe("basic user flow", () => {	
-	test("homepage should have navigation to both games", async () => {
+	test("homepage should have navigation to all pages", async () => {
 		await page.goto("http://localhost:3000/source/homepage.html");
 
 		const matchingButton = await page.$("#matching-button");
@@ -22,6 +22,18 @@ describe("basic user flow", () => {
 			page.click("#sequence-button"),
 		]);
 		expect(page.url()).toBe("http://localhost:3000/source/sequence.html");
+
+
+		await page.goto("http://localhost:3000/source/homepage.html");
+
+		const leaderboardButton = await page.$("#leaderboard-btn");
+		expect(leaderboardButton).not.toBeNull();
+
+		await Promise.all([
+			page.waitForNavigation(),
+			page.click("#leaderboard-btn"),
+		]);
+		expect(page.url()).toBe("http://localhost:3000/source/leaderboard.html");
 	});
 
 	test("matching game should allow interaction", async () => {
@@ -33,19 +45,25 @@ describe("basic user flow", () => {
 		await page.click("#start-btn");
 		await page.waitForSelector("#start-btn", { hidden: true });
 
-		// await cards[0].click();
-		// const flippedCard = await page.$(".flipped");
-		// expect(flippedCard).toBe(cards[0]);
+		const firstCard = cards[0];
+		await firstCard.click();
+		const firstCardImage = await firstCard.getProperty("src");
+		expect(firstCardImage).not.toBe("../assets/G14.png");
 	});
 
 	test("sequence game should allow interaction", async () => {
 		await page.goto("http://localhost:3000/source/sequence.html");
 
-		// const cards = await page.$$(".card");
-		// expect(cards.length).toBe(16);
-
 		await page.click("#start-btn");
 		await page.waitForSelector("#start-btn", { hidden: true });
+
+		const cards = await page.$$(".card");
+		expect(cards.length).toBe(9);
+
+		const firstCard = cards[0];
+		await firstCard.click();
+		const flippedCard = await page.waitForSelector(".flipped")
+			.then(expect(flippedCard).toBe(firstCard));
 	});
 });
 
