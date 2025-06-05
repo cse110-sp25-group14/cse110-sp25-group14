c483317d15cf7c1658ac98739a7e3aa3f632e797
Andy <kgh3122@gmail.com>
2025-06-04 18:21:36 -0700

    added record.js

---
+++ scripts/record.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', init);
+
+function init()
+{
+    const backButton = document.getElementById("back-arrow");
+
+    backButton.addEventListener("click", () => {
+        window.location.href = "homepage.html";
+    });
+
+    if(localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark');
+    }
+}
