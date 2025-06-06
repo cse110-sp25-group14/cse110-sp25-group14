6f1a186c04f20eeadb6b3d7b7edc029a78e7e071
yscmark <124672723+yscmark@users.noreply.github.com>
2025-06-05 21:47:33 -0700

    Changed indentation to pass linter

--- scripts/records.js
+++ scripts/records.js
@@ -6,52 +6,52 @@ function init() {
 		window.location.href = "homepage.html";
 	});
 
-    const recordDropdown = document.getElementById("record-dropdown");
+	const recordDropdown = document.getElementById("record-dropdown");
 
-    const matchingContainer = document.getElementById("matching-records");
-    const sequenceContainer = document.getElementById("sequence-records");
+	const matchingContainer = document.getElementById("matching-records");
+	const sequenceContainer = document.getElementById("sequence-records");
 
-    const sortByDropdown = document.getElementById("sort-dropdown");
-    const difficultyDropdown = document.getElementById("difficulty-dropdown");
+	const sortByDropdown = document.getElementById("sort-dropdown");
+	const difficultyDropdown = document.getElementById("difficulty-dropdown");
 
-    matchingContainer.style.display = "none";
-    sequenceContainer.style.display = "none";
+	matchingContainer.style.display = "none";
+	sequenceContainer.style.display = "none";
 
-    sortByDropdown.style.display = "none";
-    difficultyDropdown.style.display = "none";
+	sortByDropdown.style.display = "none";
+	difficultyDropdown.style.display = "none";
 
-    const dropdownHeader = document.getElementById("best-title");
-    dropdownHeader.style.display = "none";
+	const dropdownHeader = document.getElementById("best-title");
+	dropdownHeader.style.display = "none";
 
-    recordDropdown.addEventListener("change", (e) => {
-        const value = e.target.value;
+	recordDropdown.addEventListener("change", (e) => {
+		const value = e.target.value;
 
-        if (value === "matching") {
-            matchingContainer.style.display = "grid";
-            sequenceContainer.style.display = "none";
+		if (value === "matching") {
+			matchingContainer.style.display = "grid";
+			sequenceContainer.style.display = "none";
 
-            sortByDropdown.style.display = "block";
-            difficultyDropdown.style.display = "none";
-            dropdownHeader.style.display = "";
-            dropdownHeader.textContent = "Best Matches";
+			sortByDropdown.style.display = "block";
+			difficultyDropdown.style.display = "none";
+			dropdownHeader.style.display = "";
+			dropdownHeader.textContent = "Best Matches";
 
-        } else if (value === "sequence") {
-            matchingContainer.style.display = "none";
-            sequenceContainer.style.display = "grid";
+		} else if (value === "sequence") {
+			matchingContainer.style.display = "none";
+			sequenceContainer.style.display = "grid";
 
-            sortByDropdown.style.display = "none";
-            difficultyDropdown.style.display = "block";
-            dropdownHeader.style.display = "";
-            dropdownHeader.textContent = "Difficulty";
+			sortByDropdown.style.display = "none";
+			difficultyDropdown.style.display = "block";
+			dropdownHeader.style.display = "";
+			dropdownHeader.textContent = "Difficulty";
 
-        } else {
-            matchingContainer.style.display = "none";
-            sequenceContainer.style.display = "none";
+		} else {
+			matchingContainer.style.display = "none";
+			sequenceContainer.style.display = "none";
 
-            sortByDropdown.style.display = "none";
-            difficultyDropdown.style.display = "none";
+			sortByDropdown.style.display = "none";
+			difficultyDropdown.style.display = "none";
 
-            dropdownHeader.style.display = "none";
-        }
-    });
+			dropdownHeader.style.display = "none";
+		}
+	});
 }
