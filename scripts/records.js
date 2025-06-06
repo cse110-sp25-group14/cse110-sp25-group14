330464c606b13da27b6190631dec931e07c9b17c
srideep9 <dornala.srideep@gmail.com>
2025-06-05 23:04:49 -0700

    Update records.js

--- scripts/records.js
+++ scripts/records.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", init);
 function init() {
 
-	const backButton = document.getElementById("page-info");
+        const backButton = document.getElementById("page-info");
 	backButton.querySelector("img").addEventListener("click", ()=>{
 		window.location.href = "homepage.html";
 	});
