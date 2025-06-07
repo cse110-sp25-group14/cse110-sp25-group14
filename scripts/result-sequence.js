938de63244ef5e174480c9ea3b461ce7f2902a2e
gautammdas13 <gmohandas2@student.ohlone.edu>
2025-06-06 17:09:57 -0700

    Created result-sequence.js to add results page functionality. Needs to be tested

---
+++ scripts/result-sequence.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.getItem("darkMode") === "enabled") {
+        document.body.classList.add("dark");
+    }
+
+    const backButton = document.querySelector("#page-info img");
+    backButton.addEventListener("click", () => {
+        window.location.href = "../homepage.html";
+    });
+
+    const recent = JSON.parse(localStorage.getItem("sequence-recent"));
+
+    if (recent) {
+        document.querySelector(".current").textContent = `Current: ${recent.level}`;
+
+        const sequenceRecords = JSON.parse(localStorage.getItem("sequence")) || [];
+        const difficultyRecords = sequenceRecords
+		    .filter((record) => record.difficulty === recent.difficulty)
+		    .map((record) => record.level)
+		    .sort((a, b) => b - a);
+
+        const best = difficultyRecords[0] ?? 0;
+        document.querySelector(".record").textContent = `Record: ${best}`;
+    }
+});
